ff02815f65b2467ecaf04aa6bd0258e8a9952f78
ibxprofiletest <ibxprofiletest@gmail.com>
2025-07-10 05:54:47 +0900

    new

--- _nuxt/D8kzmJ3G.js
+++ _nuxt/D8kzmJ3G.js
@@ -1,1 +1,107 @@
-import{a as t,__tla as o}from"./Ct4PKcG_.js";import{k as e}from"./CkGjpDBV.js";let a,r=Promise.all([(()=>{try{return o}catch{}})()]).then(async()=>{a=e({__name:"privacy-policy",setup(l){return t({title:"Crystal Agency | Privacy Policy",meta:[{name:"description",content:"Crystal Agency privacy policy page."}]}),(i,c)=>null}})});export{r as __tla,a as default};
+import { u as l, o as c, a as g, _ as h, __tla as d } from "./Ct4PKcG_.js";
+import { C as m, a as u, __tla as p } from "./BFhj9t_F.js";
+import { E as v, F as w, L as n, J as _, H as f } from "./CkGjpDBV.js";
+import { __tla as D } from "./DTeoW5xf.js";
+import "./DNkUmkFf.js";
+let s,
+  b = Promise.all([
+    (() => {
+      try {
+        return d;
+      } catch {}
+    })(),
+    (() => {
+      try {
+        return p;
+      } catch {}
+    })(),
+    (() => {
+      try {
+        return D;
+      } catch {}
+    })(),
+  ]).then(async () => {
+    let a;
+    (a = { class: "case-page" }),
+      (s = {
+        __name: "privacy-policy",
+        setup(y) {
+          const i = l();
+          v(() => {
+            i.hideCursor("", "none");
+          });
+          const e = {
+            name: "Privacy Policy",
+            description:
+              "Read our privacy policy to understand how we collect, use, and protect your information",
+              tags: ["Privacy", "Policy", "Legal"],
+            awards: !1,
+            link: "/",
+            video: "/",
+            buttonText: "TRY IT<br> NOW",
+            nextCase: "/",
+            videoPoster: "/images/cus/Configurator.png",
+          };
+          return (
+            c((o, r, t) => {
+              i.startTransition(o.path, r.path, t);
+            }),
+            g({
+              title:
+                "Crystal Agency | Privacy Policy",
+              meta: [
+                {
+                  name: "description",
+                  content:
+                    "Read our privacy policy to understand how we collect, use, and protect your information",
+                },
+                {
+                  name: "og:title",
+                  property: "og:title",
+                  content:
+                    "Crystal Agency | Privacy Policy",
+                },
+                {
+                  name: "og:description",
+                  property: "og:description",
+                  content:
+                    "Read our privacy policy to understand how we collect, use, and protect your information",
+                },
+                {
+                  name: "twitter:title",
+                  content:
+                    "Crystal Agency | Privacy Policy",
+                },
+                {
+                  name: "twitter:description",
+                  content:
+                    "Read our privacy policy to understand how we collect, use, and protect your information",
+                },
+              ],
+            }),
+            (o, r) => {
+              const t = h;
+              return (
+                f(),
+                w("div", a, [
+                  n(t, null, {
+                    default: _(() => [
+                      n(
+                        m,
+                        { video: { video: e.video, poster: e.videoPoster } },
+                        null,
+                        8,
+                        ["video"]
+                      ),
+                    ]),
+                    _: 1,
+                  }),
+                  n(u, { page: e }),
+                ])
+              );
+            }
+          );
+        },
+      });
+  });
+export { b as __tla, s as default };
